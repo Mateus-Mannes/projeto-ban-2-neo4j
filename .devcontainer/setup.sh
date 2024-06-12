38697bcae1e7094bf5547ffc639a23ba6dcf8da5
Mateus Mannes <mateus-mannes@hotmail.com>
2024-06-11 21:17:05 -0300

    alterando setup para criar um bando neo4j

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,4 +1,1 @@
-export PGPASSWORD='postgres'
-psql -U postgres -h db -c "CREATE DATABASE gv;"
-psql -U postgres -h db -d gv -a -f  create_tables.sql
-psql -U postgres -h db -d gv -a -f  data_seed.sql
+cypher-shell -f import_data.cypher
